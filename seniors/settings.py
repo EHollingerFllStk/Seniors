7258989d1d98e9e9bf4c6661a77e7f96b41afa80
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2022-10-21 10:40:21 -0400

    set up routes and migrate db

--- seniors/settings.py
+++ seniors/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'seniors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'seniors',
     }
 }
 
