073157d210da5913f09a1f3da71c3595355a6f44
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2022-10-21 12:43:38 -0400

    add index.html to agencies

--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,19 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def agencies_index(request):
+  return render(request, 'agencies/index.html', { 'agencies': agencies })
+
+class Agencies:
+  def __init__(self, name, address, phone, rating):
+    self.name = name
+    self.address = address
+    self.phone = phone
+    self.rating = rating
+
+agencies = [
+  Agencies('First Light Home Care','555 Bryn Mawr Ave. Bryn Mawr, PA 19634', '610-638-0638', 3),
+  Agencies('Visiting Angels Home Care','521 Bryn Mawr Ave. Bryn Mawr, PA 19634', '610-638-0638', 3),
+  Agencies('Senior Living Home Health Care','523 Bryn Mawr Ave. Bryn Mawr, PA 19634', '610-638-0638', 4),
+]
