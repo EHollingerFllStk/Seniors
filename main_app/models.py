c86f5c2f37b1a9cb3c062852242149b0e8dd32a2
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2022-10-23 16:18:00 -0400

    add user for auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+from django.contrib.auth.models import User
 
 SERVICES = (
     ('B', 'Bathing'),
@@ -19,6 +20,7 @@ class Agencies(models.Model):
     address = models.TextField(max_length=250)
     phone = models.CharField(max_length=100)
     rating = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
